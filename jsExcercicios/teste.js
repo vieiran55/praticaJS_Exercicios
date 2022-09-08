a04b0d4502ff3c49fa27bb763a7cf6810cc1eec4
Antonio Vieira <vieira.n55@gmail.com>
2022-09-08 15:32:15 -0300

    node criando bibliotecas

---
+++ jsExcercicios/teste.js
@@ -0,0 +1,9 @@
+function verificarPalavra(string){
+  if (string.includes('olá') == true){
+    console.log('a palavra está contida na frase')
+  }else{
+      console.log('a palavra não está contida na frase')
+  }
+}
+
+verificarPalavra('olá')
